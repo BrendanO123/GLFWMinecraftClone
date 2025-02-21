b1b4fcab77abbdc5b198925a9fd5ba7a2da8a1c3
BrendanO <brendan2520@icloud.com>
2025-02-21 10:51:05 -0500

    turned off debug generation and reduced chunkdata size

--- MinecraftClone/src/WorldGen.cpp
+++ MinecraftClone/src/WorldGen.cpp
@@ -70,11 +70,23 @@ void WorldGen :: getChunkBasics(int x, int z, int chunkSize, ChunkData* chunkDat
     }
     if(maxHeight < noise :: Fractal :: waterLevel){maxHeight = noise :: Fractal :: waterLevel;}
 
-    chunkData->data.reserve(maxHeight);
-    for(int i = 0; i < maxHeight; i++){
-        chunkData->data.emplace_back((unsigned char)i);
-        if(i < minHeight-3){
-            if(fullStone){fillLayer(i, chunkSize, chunkData, Blocks :: STONE);}
+    constexpr int partialStone_Thickness = 7;
+    int lowest;
+    if(fullStone){
+        lowest = 0;
+        chunkData->data.reserve(maxHeight);
+        for(int i = 0; i < maxHeight; i++){
+            chunkData->data.emplace_back((unsigned char)i);
+            if(i < minHeight-3){
+                fillLayer(i, chunkSize, chunkData, Blocks :: STONE);
+            }
+        }
+    }
+    else{
+        lowest = max(0, minHeight-partialStone_Thickness);
+        chunkData->data.reserve(maxHeight - lowest);
+        for(int i = lowest; i < maxHeight; i++){
+            chunkData->data.emplace_back((unsigned char)i);
         }
     }
 
@@ -85,28 +97,38 @@ void WorldGen :: getChunkBasics(int x, int z, int chunkSize, ChunkData* chunkDat
                     chunkData->data.at(i).data[iterZ + (iterX<<4)] = Blocks :: STONE;
                 }
             }
-            for(int i = noiseValues.TerrainHeight[iterZ + int(iterX << 4)] - 3; i < noise :: Fractal :: waterLevel; i++){
-                    chunkData->data.at(i).data[iterZ + (iterX<<4)] = Blocks :: WATER;
-                }
-
-            if(noiseValues.surfaceType[iterZ + int(iterX << 4)] == Blocks :: GRASS){
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-3).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-2).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1).data[iterZ + (iterX<<4)] = Blocks :: GRASS;
-                
-                
-                if(iterX == 0 || iterX == 15 || iterZ == 0 || iterZ == 15){
-                    chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
+            else{
+                for(int i = max(0, noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-partialStone_Thickness) - lowest; i<noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-3 - lowest; i++){
+                    chunkData->data.at(i).data[iterZ + (iterX<<4)] = Blocks :: STONE;
                 }
             }
+
+            for(int i = noiseValues.TerrainHeight[iterZ + int(iterX << 4)] - lowest; i < noise :: Fractal :: waterLevel - lowest; i++){
+                    chunkData->data.at(i).data[iterZ + (iterX<<4)] = Blocks :: WATER;
+            }
+
+            if(noiseValues.surfaceType[iterZ + int(iterX << 4)] == Blocks :: GRASS){
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-3 - lowest).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-2 - lowest).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1 - lowest).data[iterZ + (iterX<<4)] = Blocks :: GRASS;
+                
+                
+                /*if(iterX == 0 || iterX == 15 || iterZ == 0 || iterZ == 15){
+                    chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
+                }*/
+            }
             else{
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-3).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-2).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
-                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-3 - lowest).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-2 - lowest).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
+                chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1 - lowest).data[iterZ + (iterX<<4)] = noiseValues.surfaceType[iterZ + int(iterX << 4)];
+
+                /*if(iterX == 0 || iterX == 15 || iterZ == 0 || iterZ == 15){
+                    chunkData->data.at(noiseValues.TerrainHeight[iterZ + int(iterX << 4)]-1).data[iterZ + (iterX<<4)] = Blocks :: DIRT;
+                }*/
             }
         }
     }
-    chunkData->data.at(noiseValues.TerrainHeight[1 + int(2 << 4)]-1).data[1 + (2<<4)] = Blocks :: STONE;
+    //chunkData->data.at(noiseValues.TerrainHeight[1 + int(2 << 4)]-1).data[1 + (2<<4)] = Blocks :: STONE;
     
 
     for(int iterX = 0; iterX < chunkSize; iterX++){
